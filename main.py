5df73cbcb01bbdd0580081cd1dca8cf992b9f75f
Meandr58 <andrey.mescherin@gmail.com>
2024-06-17 22:04:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ print("Привет!")
 input("Как тебя зовут?")
 list1 = [1, 2, 3, 4, 5, 12, 23]
 list2 = [4, 5, 6, 7, 8, 23, 24]
-set1 = set()
-set2 = set()
+set1 = set()s
+set2 = set()s
 for item in list1:
     set1.add(item)
 for item in list2:
